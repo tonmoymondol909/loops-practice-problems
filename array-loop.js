42b7443ae6c4fcd8937e9b56deef3ce9baef551d
Tonmoy <tonmoymondol2614@gmail.com>
2024-01-26 20:01:14 +0600

    bottle mouse sunglass pen notebook

--- array-loop.js
+++ array-loop.js
@@ -3,7 +3,7 @@
 // }
 
 // target display every elements of an array
-// var numbers =[45, 87, 89, 56, 32, 51, 25, 188, 41, 25, 98];
+var numbers =[45, 87, 89, 56, 32, 51, 25, 188, 41, 25, 98];
 
 // for(var i =0; i < numbers.length; i++){
 //     var number = numbers[i];
