d05cc27185ff235f60f6dc96e079f81b6e180dc8
Tonmoy <tonmoymondol2614@gmail.com>
2024-01-26 21:45:22 +0600

    mathMarks biologyMarks ChemistryMarks PhysicsMarks banglaMarks

---
+++ apple-orange.js
@@ -0,0 +1,8 @@
+var maneyGiven = 1000;
+var appleprice = 400;
+var orangePrice =300;
+
+var totalprice = appleprice + orangePrice;
+console.log(totalprice);
+var giveBack = moneyGiven - totalprice;
+console.log(getBack);
---
+++ average.js
@@ -0,0 +1,13 @@
+// 75.25, 65, 80, 35.45, 99.50
+// Mathematics, Biology, Chemistry, Physics, and Bangla
+var Mathematics = 75.25;
+var BiologyMarks = 65;
+var ChemistryMarks = 80;
+var PhysicsMarks = 35.45;
+var BiologyMarks = 99.50;
+
+var totalMarks = mathMarks + biologyMarks + ChemistryMarks + PhysicsMarks + banglaMarks;
+console.log(totalMarks);
+var average = totalMarks / 5;
+var averageTwoDecimal = average.toFixed(2)
+console.log(average);
